c075eef55b1e8f68df6f34441a84ac7042eac6d2
6congyao <8966098@qq.com>
2024-03-11 04:49:36 +0800

    add filter

--- conversation.go
+++ conversation.go
@@ -1,13 +1,23 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var template = "Dear %s, Your unwavering support and trust in our products/services mean the world to us. We are truly grateful for the opportunity to serve you and for the strong partnership we have built."
+
 func parseEvent(event FacebookEvent) error {
 	if event.Field == "messages" {
 		//Make some call to ChatGPT asking if the comment posted on the
 		//Page suggests user wants to give a review
 		// callChatGPTToCheckIfMessageSuggestReview(event.Message) returns boolean
 		if true { //Swap this out based on ChatGPT response
-			return sendMessage("Please feel free to provide a review about our Page!",
-				event.CustomerId, "RESPONSE")
+			message, _err := generateResponseMessage(event.Message, event.CustomerId)
+			if _err == nil {
+				return sendMessage(message, event.CustomerId, "RESPONSE")
+			}
 		}
 	} else {
 		// review := event.Message
@@ -16,4 +26,11 @@ func parseEvent(event FacebookEvent) error {
 		return nil
 	}
 	return nil
+}
+
+func generateResponseMessage(message string, customerId string) (string, error) {
+	if strings.Contains(message, "thank") {
+		return fmt.Sprintf(template, customerId), nil
+	}
+	return "", errors.New("no need to reply")
 }
